f40e2f927a60066292fa1ef8c5a474533f7021c7
Arthur St <artrexwow@gmail.com>
2023-10-04 14:33:28 -0300

    Added save data service

---
+++ Assets/_Project/Source/USE.DataService/ISaveDataService.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace USE.DataService
+{
+    public interface ISaveDataService
+    {
+        public GameData GameData { get; }
+        public void Initialize(string fileName, bool useEncryption);
+        void SaveData(GameData data);
+        public void SaveGame();
+    }
+}
---
+++ Assets/_Project/Source/USE.DataService/SaveDataService.cs
@@ -0,0 +1,56 @@
+using Coimbra;
+using UnityEngine;
+
+namespace USE.DataService
+{
+    public sealed class SaveDataService : Actor, ISaveDataService
+    {
+        public GameData GameData { get; private set; }
+        private bool _useEncryption;
+        private FileDataHandler _fileDataHandler;
+
+        public void Initialize(string fileName, bool useEncryption)
+        {
+            _useEncryption = useEncryption;
+#if UNITY_EDITOR
+            _fileDataHandler = new FileDataHandler(Application.dataPath + "/Saves/", fileName, _useEncryption);
+#else
+            _fileDataHandler = new FileDataHandler(Application.persistentDataPath, fileName, _useEncryption);
+#endif
+            LoadGame();
+        }
+
+        public void SaveData(GameData data)
+        {
+            GameData = data;
+        }
+
+        public void SaveGame()
+        {
+            _fileDataHandler.Save(GameData);
+        }
+
+        private void NewGame()
+        {
+            GameData = new GameData();
+        }
+
+        private void LoadGame()
+        {
+            GameData = _fileDataHandler.Load();
+
+            if(GameData == null)
+            {
+                Debug.Log($"<color=green>New Game</color>, set all to default");
+                NewGame();
+            }
+        }
+
+        private new void OnApplicationQuit()
+        {
+            base.OnApplicationQuit();
+            SaveGame();
+        }
+    }
+}
+
