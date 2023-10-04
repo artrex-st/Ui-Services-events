73391b6c288f7d99746246fd8806e4576162cc84
Arthur St <artrexwow@gmail.com>
2023-10-04 19:00:16 -0300

    Game scenes flow placeholder

--- Assets/_Project/Source/MainMenuController.cs
+++ Assets/_Project/Source/MainMenuController.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using System;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,7 +10,9 @@ namespace Source
     {
         [SerializeField] private Button _PlayButton;
         [SerializeField] private Button _settingsButton;
+        [SerializeField] private Button _exitGameButton;
         [Header("Screen Reference")]
+        [SerializeField] private ScreenReference _gameScreenRef;
         [SerializeField] private ScreenReference _settingsScreenRef;
 
         private void OnEnable()
@@ -27,22 +30,33 @@ namespace Source
             base.Initialize();
             _PlayButton.onClick.AddListener(PlayButtonClickHandler);
             _settingsButton.onClick.AddListener(SettingsButtonClickHandler);
+            _exitGameButton.onClick.AddListener(ExitGameButtonClickHandler);
 
             SoundService.PlayMusic(SoundService.SoundLibrary.MainMenuMusic);
         }
 
         private void PlayButtonClickHandler()
         {
-// #if UNITY_EDITOR
-//             UnityEditor.EditorApplication.isPlaying = false;
-// #else
-//             Application.Quit();
-// #endif
+            AsyncOperation openSceneOperationAsync = ScreenService.LoadSingleSceneAsync(_gameScreenRef);
+            openSceneOperationAsync.completed += async operation =>
+            {
+                await UniTask.Delay(TimeSpan.FromSeconds(3));
+                AsyncOperation backSceneAsync = ScreenService.LoadSingleSceneAsync(_thisScreenRef);
+            };
         }
 
         private void SettingsButtonClickHandler()
         {
             AsyncOperation openSceneOperationAsync = ScreenService.LoadAdditiveSceneAsync(_settingsScreenRef);
+        }
+
+        private void ExitGameButtonClickHandler()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
         }
     }
 }
